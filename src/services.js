6d8fb08ea50612cfcaa0b2c33ade575cfba5ed06
Dang Nguyen <haidang009@gmail.com>
2023-06-24 17:56:45 +0700

    Add nginx

--- src/services.js
+++ src/services.js
@@ -1,14 +1,14 @@
 const { default: Storage } = require('@file-storage/core');
 const { FileUploadModel } = require('./db');
+const { defaultDiskName } = require('./storage');
 
-async function upload(data, fileName, disk = 'local') {
-  const storage = disk === 'local' ? Storage.disk('local') : Storage;
-  const result = await storage.put(data, 'upload/' + fileName);
+async function upload(data, fileName, diskName = defaultDiskName) {
+  const result = await Storage.disk(diskName).put(data, 'upload/' + fileName);
 
   return FileUploadModel.create({
     name: result.name,
     path: result.path,
-    disk: storage.name,
+    disk: diskName,
     ...(result.formats && {
       formats: result.formats,
     }),
